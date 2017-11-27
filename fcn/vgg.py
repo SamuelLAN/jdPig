0fb12a40e6b9be1aa49776e113b9d421940aef56
Yusen Lin <412206186@qq.com>
2017-11-27 16:22:26 +0800

    [RC] modify vgg

--- fcn/vgg.py
+++ fcn/vgg.py
@@ -8,6 +8,7 @@ from six.moves.urllib.request import urlretrieve
 
 ''' VGG 模型 (16层版) '''
 class VGG:
+    MODEL_DIR = r'model'
     MODEL = r'model/vgg16.npy'
     MODEL_URL = r'http://www.lin-baobao.com/model/vgg16.npy'
 
@@ -22,6 +23,8 @@ class VGG:
         Returns:
             vgg_mode (dict)
         '''
+        if not os.path.isdir(VGG.MODEL_DIR):
+            os.mkdir(VGG.MODEL_DIR)
         if not os.path.isfile(VGG.MODEL):
             print 'Start downloading %s' % VGG.MODEL
             file_path, _ = urlretrieve(VGG.MODEL_URL, VGG.MODEL, reporthook=VGG.__download_progress)
