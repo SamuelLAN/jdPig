d54d74122663388261fecd137e988927abe2c608
Yusen Lin <412206186@qq.com>
2017-11-28 16:40:48 +0800

    [RC] debug fcn output mask

--- fcn/fcn.py
+++ fcn/fcn.py
@@ -20,7 +20,7 @@ class FCN(base.NN):
     MODEL_NAME = 'fcn'  # 模型的名称
 
     BATCH_SIZE = 12     # 迭代的 epoch 次数
-    EPOCH_TIMES = 1    # 随机梯度下降的 batch 大小
+    EPOCH_TIMES = 5    # 随机梯度下降的 batch 大小
 
     IMAGE_SHAPE = [320, 180]
     IMAGE_PIXELS = IMAGE_SHAPE[0] * IMAGE_SHAPE[1]
