e312eebfb8a166e29714f0af4a7049f20f003bb5
Yusen Lin <412206186@qq.com>
2017-11-30 02:08:23 +0800

    [DF] reset

--- fcn/base.py
+++ fcn/base.py
@@ -2,11 +2,13 @@
 # -*- coding: utf-8 -*-
 import tensorflow as tf
 from math import sqrt
+import numpy as np
 from numpy import hstack
 import re
 import sys
 import os
 from multiprocessing import Process
+
 
 '''
  网络的基类
@@ -44,22 +46,20 @@ from multiprocessing import Process
     他常用函数 (与 DL 无关)
       echo
 '''
-
-
 class NN:
-    MODEL_NAME = 'model_name'  # 模型的名称
+    MODEL_NAME = 'model_name'           # 模型的名称
 
-    EPOCH_TIMES = 100  # 迭代的 epoch 次数
-    BATCH_SIZE = 100  # 随机梯度下降的 batch 大小
+    EPOCH_TIMES = 100                   # 迭代的 epoch 次数
+    BATCH_SIZE = 100                    # 随机梯度下降的 batch 大小
 
-    BASE_LEARNING_RATE = 0.1  # 初始 学习率
-    DECAY_RATE = 0.95  # 学习率 的 下降速率
+    BASE_LEARNING_RATE = 0.1            # 初始 学习率
+    DECAY_RATE = 0.95                   # 学习率 的 下降速率
 
-    DROPOUT_RATE = 0.8  # 卷机神经网络 使用的 dropout 配置
-    DROPOUT_LIST = None  # 全连接模型 使用的 dropout 配置
-    MODEL = []  # 深度模型的配置
+    DROPOUT_RATE = 0.8                  # 卷机神经网络 使用的 dropout 配置
+    DROPOUT_LIST = None                 # 全连接模型 使用的 dropout 配置
+    MODEL = []                          # 深度模型的配置
 
-    TENSORBOARD_SHOW_IMAGE = False  # 默认不将 image 显示到 TensorBoard，以免影响性能
+    TENSORBOARD_SHOW_IMAGE = False      # 默认不将 image 显示到 TensorBoard，以免影响性能
 
     DEBUG = True
 
@@ -68,74 +68,72 @@ class NN:
     def __init__(self):
         self.__init()
 
-    ''' 析构函数 '''
 
+    ''' 析构函数 '''
     def __del__(self):
         NN.kill_tensorboard_if_runing()
         # self.tbProcess.join(10)
         self.tbProcess.terminate()
 
-    ''' 初始化 '''
 
+    ''' 初始化 '''
     def __init(self):
-        self.net = []  # 存放每层网络的 feature map
-        self.WList = []  # 存放权重矩阵的 list
-        self.bList = []  # 存放偏置量的 list
+        self.net = []                                       # 存放每层网络的 feature map
+        self.WList = []                                     # 存放权重矩阵的 list
+        self.bList = []                                     # 存放偏置量的 list
 
         self.__modelPath = ''
-        self.__get_model_path()  # 生成存放模型的文件夹 与 路径
+        self.__get_model_path()                             # 生成存放模型的文件夹 与 路径
 
         self.__summaryPath = ''
         self.__get_summary_path()
 
-        self.globalStep = self.get_global_step()  # 记录全局训练状态的 global step
+        self.globalStep = self.get_global_step()            # 记录全局训练状态的 global step
 
-        self.init()  # 执行定制化的 初始化操作
+        self.init()                                         # 执行定制化的 初始化操作
 
-        self.saver = tf.train.Saver()
         self.sess = tf.Session()
 
     # ******************************* 子类需要实现的接口 *******************************
 
     ''' 初始化各种 变量 常量 '''
-
     def init(self):
         pass
 
-    ''' 加载数据 '''
 
+    ''' 加载数据 '''
     def load(self):
         pass
 
-    ''' 模型 '''
 
+    ''' 模型 '''
     def model(self):
         pass
 
-    ''' 前向推导 '''
 
+    ''' 前向推导 '''
     def inference(self):
         pass
 
-    ''' 计算 loss '''
 
+    ''' 计算 loss '''
     def get_loss(self):
         pass
 
-    ''' 主函数 '''
 
+    ''' 主函数 '''
     def run(self):
         pass
 
     # *************************** 初始化变量 ****************************
 
     ''' 初始化所有变量 '''
-
     def init_variables(self):
         self.sess.run(tf.global_variables_initializer())
+        self.saver = tf.train.Saver()  # 初始化 saver; 用于之后保存 网络结构
+
 
     ''' 初始化权重矩阵 '''
-
     @staticmethod
     def init_weight(shape):
         if len(shape) == 4:
@@ -151,8 +149,14 @@ class NN:
             name='weight'
         )
 
-    ''' 初始化 bias '''
 
+    ''' 初始化权重矩阵 '''
+    @staticmethod
+    def init_weight_w(w, trainable=True):
+        return tf.Variable(w, trainable=trainable, name='weight')
+
+
+    ''' 初始化 bias '''
     @staticmethod
     def init_bias(shape):
         if len(shape) == 4:
@@ -162,21 +166,34 @@ class NN:
 
         return tf.Variable(tf.zeros([nodes]), name='bias')
 
-    ''' 获取全局的训练 step '''
 
+    ''' 初始化 bias '''
+    @staticmethod
+    def init_bias_b(b, trainable=True):
+        return tf.Variable(b, trainable=trainable, name='bias')
+
+
+    @staticmethod
+    def get_variable(weights, name):
+        init = tf.constant_initializer(weights, dtype=tf.float32)
+        var = tf.get_variable(name=name, initializer=init, shape=weights.shape)
+        return var
+
+
+    ''' 获取全局的训练 step '''
     @staticmethod
     def get_global_step():
         return tf.Variable(0, name='global_step', trainable=False)
 
-    ''' 获取随迭代次数下降的学习率 '''
 
+    ''' 获取随迭代次数下降的学习率 '''
     @staticmethod
     def get_learning_rate(base_learning_rate, cur_step, decay_times, decay_rate=0.95, staircase=False):
         learning_rate = tf.train.exponential_decay(
-            base_learning_rate,  # Base learning rate.
-            cur_step,  # Current index into the dataset.
-            decay_times,  # Decay step.
-            decay_rate,  # Decay rate.
+            base_learning_rate,     # Base learning rate.
+            cur_step,               # Current index into the dataset.
+            decay_times,            # Decay step.
+            decay_rate,             # Decay rate.
             staircase=staircase
         )
         tf.summary.scalar('learning_rate', learning_rate)
@@ -185,17 +202,24 @@ class NN:
     # *************************** 保存模型 ***************************
 
     ''' 保存模型 '''
-
     def save_model(self):
         self.saver.save(self.sess, self.__get_model_path())
-
+        
+    
     ''' 恢复模型 '''
-
     def restore_model(self):
-        self.saver.restore(self.sess, self.__get_model_path())
+        model_path = self.__get_model_path()
+        self.saver = tf.train.import_meta_graph('%s.meta' % model_path)
+        self.saver.restore(self.sess, tf.train.latest_checkpoint(os.path.split(model_path)[0]))
+        self.graph = self.sess.graph
+
+
+    ''' 根据 name 获取 tensor 变量 '''
+    def get_variable_by_name(self, name):
+        return self.graph.get_tensor_by_name(name)
+
 
     ''' 获取存放模型的路径 '''
-
     def __get_model_path(self):
         if self.__modelPath:
             return self.__modelPath
@@ -246,21 +270,21 @@ class NN:
         self.__summaryWriterTrain.close()
         self.__summaryWriterVal.close()
 
-    ''' 输出前 num 个节点的图像到 TensorBoard '''
 
+    ''' 输出前 num 个节点的图像到 TensorBoard '''
     def image_summary(self, tensor_4d, num, name, i):
         if not self.TENSORBOARD_SHOW_IMAGE:
             return
 
         with tf.name_scope('summary'):
-            index = self.globalStep % self.BATCH_SIZE  # 让每次输出不同图片，取不同 index 的图片
-            shape = list(hstack(
-                [-1, [int(j) for j in tensor_4d.shape[1: 3]], 1]))  # 生成的 shape 为 [-1, image_width, image_height, 1]
-            image = tf.concat([tensor_4d[index, :, :, j] for j in range(num)], 0)  # 将多幅图像合并在一起
+            index = self.globalStep % self.BATCH_SIZE       # 让每次输出不同图片，取不同 index 的图片
+            shape = list(hstack([-1, [int(j) for j in tensor_4d.shape[1: 3]], 1]))  # 生成的 shape 为 [-1, image_width, image_height, 1]
+            image = tf.concat([tensor_4d[index, :, :, j] for j in range(num)], 0)   # 将多幅图像合并在一起
 
             # 必须 reshape 成 [?, image_size, image_size, 1 或 3 或 4]
             image = tf.reshape(image, shape)
             tf.summary.image('%s_%d' % (name, i), image)
+
 
     @staticmethod
     def activation_summary(var):
@@ -269,11 +293,13 @@ class NN:
                 tf.summary.histogram(var.op.name + '/activation', var)
                 tf.summary.scalar(var.op.name + '/sparsity', tf.nn.zero_fraction(var))
 
+
     @staticmethod
     def gradient_summary(grad, var):
         if type(grad) != type(None):
             with tf.name_scope('gradient'):
                 tf.summary.histogram(var.op.name + 'gradient', grad)
+
 
     ''' 获取 summary path '''
 
@@ -307,6 +333,7 @@ class NN:
         self.run_tensorboard(self.__summaryPath)
         return self.__summaryPath
 
+
     @staticmethod
     def __remove_file_recursive(dir_path):
         for file_name in os.listdir(dir_path):
@@ -318,8 +345,8 @@ class NN:
     def cmd(command):
         return os.popen(command).read()
 
-    ''' 若 tensorboard 正在运行, kill tensorboard 进程 '''
 
+    ''' 若 tensorboard 正在运行, kill tensorboard 进程 '''
     @staticmethod
     def kill_tensorboard_if_runing():
         try:
@@ -339,10 +366,10 @@ class NN:
         except Exception, ex:
             print ex
 
+
     '''
      同步状态 自动在终端打开 cmd (默认端口为 6006，port 参数可以自己指定端口)
     '''
-
     @staticmethod
     def __run_tensorboard_sync(path, port=6006):
         try:
@@ -351,8 +378,8 @@ class NN:
         except Exception, ex:
             print ex
 
-    ''' 异步状态，自动在终端打开 cmd (默认端口为 6006，port 参数可以自己指定端口) '''
 
+    ''' 异步状态，自动在终端打开 cmd (默认端口为 6006，port 参数可以自己指定端口) '''
     def run_tensorboard(self, path, port=6006):
         NN.kill_tensorboard_if_runing()
         self.tbProcess = Process(target=NN.__run_tensorboard_sync, args=(path, port))
@@ -361,37 +388,37 @@ class NN:
     # **************************** 常用模型 ***************************
 
     ''' 全连接模型 '''
-
-    def full_connect_model(self, X, shape_list, is_train=True):
-        len_shape_list = len(shape_list)  # 获取 shape_list 的长度
+    def full_connect_model(self, X, shape_list, is_train = True):
+        len_shape_list = len(shape_list)            # 获取 shape_list 的长度
 
         # 训练时，需要初始化变量：权重矩阵、偏置量
         if is_train:
             for i, shape in enumerate(shape_list):
                 with tf.name_scope('layer_%d' % (i + 1,)):
-                    W = self.init_weight(shape)  # 初始化权重矩阵
-                    b = self.init_bias(shape)  # 初始化偏置量
+                    W = self.init_weight(shape)      # 初始化权重矩阵
+                    b = self.init_bias(shape)        # 初始化偏置量
 
-                    self.WList.append(W)  # 将权重矩阵存放到 公有变量 WList
-                    self.bList.append(b)  # 将偏置量存放到 公有变量 BList
+                    self.WList.append(W)            # 将权重矩阵存放到 公有变量 WList
+                    self.bList.append(b)            # 将偏置量存放到 公有变量 BList
 
         # 正向推导
         for i, shape in enumerate(shape_list):
             with tf.name_scope('layer_%d' % (i + 1,)):
-                W = self.WList[i]  # 获取权重矩阵
-                b = self.bList[i]  # 获取偏置量
+                W = self.WList[i]                   # 获取权重矩阵
+                b = self.bList[i]                   # 获取偏置量
 
-                if i == 0:  # 获取输入
+                if i == 0:                          # 获取输入
                     a = X
 
-                h = self.cal_h(a, W, b)  # 计算 h
-                if i != len_shape_list - 1:  # 若不是最后一层，使用 激活函数
+                h = self.cal_h(a, W, b)              # 计算 h
+                if i != len_shape_list - 1:         # 若不是最后一层，使用 激活函数
                     a = self.activate(h)
 
-                    if is_train and self.DROPOUT_LIST and self.DROPOUT_LIST[i]:  # 若在训练，且使用 dropout
+                    if is_train and self.DROPOUT_LIST and self.DROPOUT_LIST[i]:     # 若在训练，且使用 dropout
                         a = self.dropout(a, self.DROPOUT_LIST[i])
                 else:
                     return h
+
 
     ''' 
       深度模型
@@ -450,10 +477,9 @@ class NN:
                 'type': 'add',
                 'layer_index': 4, # layer_index 是从 0 开始的, 且必须小于当前 index
             }
-
+             
     '''
-
-    def deep_model(self, X, dropout=None, is_train=True):
+    def deep_model(self, X, dropout = None, is_train = True):
         if is_train:
             for i, config in enumerate(self.MODEL):
                 _type = config['type'].lower()
@@ -462,18 +488,22 @@ class NN:
                 # 卷积层
                 if _type == 'conv':
                     with tf.name_scope(name):
-                        W = self.init_weight(config['k_size'] + config['shape']) if not 'W' in config else config['W']
-                        b = self.init_bias(config['shape']) if not 'b' in config else config['b']
+                        trainable = True if 'trainable' not in config or config['trainable'] else False
+                        W = self.init_weight(config['k_size'] + config['shape']) \
+                            if not 'W' in config else self.init_weight_w(config['W'], trainable)
+                        b = self.init_bias(config['shape']) \
+                            if not 'b' in config else self.init_bias_b(config['b'], trainable)
                         self.WList.append(W)
                         self.bList.append(b)
 
                 # 反卷积层 (上采样 transpose conv)
                 elif _type == 'tr_conv':
                     with tf.name_scope(name):
-                        W = self.init_weight(config['k_size'] + config['shape']) if not 'W' in config else config['W']
-                        b = self.init_bias(
-                            config['shape'][:-2] + [config['shape'][-1], config['shape'][-2]]) if not 'b' in config else \
-                        config['b']
+                        trainable = True if 'trainable' not in config or config['trainable'] else False
+                        W = self.init_weight(config['k_size'] + config['shape']) \
+                            if not 'W' in config else self.init_weight_w(config['W'], trainable)
+                        b = self.init_bias(config['shape'][:-2] + [config['shape'][-1], config['shape'][-2]]) \
+                            if not 'b' in config else self.init_bias_b(config['b'], trainable)
                         self.WList.append(W)
                         self.bList.append(b)
 
@@ -490,7 +520,7 @@ class NN:
                     self.bList.append(None)
 
         a = X
-        if is_train:  # 将图像输出到 TensorBoard
+        if is_train:    # 将图像输出到 TensorBoard
             self.image_summary(a, 1, 'input', 1)
 
         self.echo('\nStart building model ...')
@@ -511,7 +541,7 @@ class NN:
                         # 将 a 输入到 tensorboard 中观察
                         # self.activation_summary(a)
 
-                    if is_train:  # 将前 3 个节点的图像输出到 TensorBoard
+                    if is_train:    # 将前 3 个节点的图像输出到 TensorBoard
                         self.image_summary(a, 3, _type, i)
 
             # 池化层
@@ -522,7 +552,7 @@ class NN:
                     else:
                         a = self.avg_pool(a, config['k_size'])
 
-                    if is_train:  # 将前 3 个节点的图像输出到 TensorBoard
+                    if is_train:    # 将前 3 个节点的图像输出到 TensorBoard
                         self.image_summary(a, 3, _type, i)
 
             # 全连接层
@@ -545,7 +575,7 @@ class NN:
                     if 'output_shape' in config:
                         output_shape = config['output_shape']
                     elif 'output_shape_index' in config:
-                        output_shape = tf.shape(self.net[config['output_shape_index']])
+                        output_shape = tf.shape( self.net[ config['output_shape_index'] ] )
                     elif 'output_shape_x' in config:
                         output_shape = tf.shape(X)
                         for j, val_j in enumerate(config['output_shape_x']):
@@ -572,33 +602,33 @@ class NN:
 
         return a
 
+
     # **************************** 与 模型有关 的 常用函数 *************************
 
     ''' 计算 h '''
-
     def cal_h(self, last_a, W, b):
         return tf.add(tf.matmul(last_a, W), b, name='h')
 
-    ''' 激活函数 '''
 
+    ''' 激活函数 '''
     def activate(self, h):
         return tf.nn.relu(h, name='a')
 
-    ''' 2D 卷积 '''
 
+    ''' 2D 卷积 '''
     @staticmethod
     def conv2d(x, W):
         return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='SAME')
 
-    ''' 2D 卷积 并加上 bias '''
 
+    ''' 2D 卷积 并加上 bias '''
     @staticmethod
     def conv2d_bias(x, W, b):
         conv = tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='SAME')
         return tf.nn.bias_add(conv, b)
 
-    ''' 2D 反卷积(transpose conv) 并 加上 bias '''
 
+    ''' 2D 反卷积(transpose conv) 并 加上 bias '''
     @staticmethod
     def conv2d_transpose_stride(x, W, b, output_shape=None, stride=2):
         # 若没有设置 output_shape
@@ -610,15 +640,15 @@ class NN:
         conv = tf.nn.conv2d_transpose(x, W, output_shape, strides=[1, stride, stride, 1], padding="SAME")
         return tf.nn.bias_add(conv, b)
 
-    ''' max pooling '''
 
+    ''' max pooling '''
     @staticmethod
     def max_pool(x, k_size):
         k_size = list(hstack([1, k_size, 1]))
         return tf.nn.max_pool(x, ksize=k_size, strides=k_size, padding='SAME')
 
-    ''' mean pooling '''
 
+    ''' mean pooling '''
     @staticmethod
     def avg_pool(x, k_size):
         k_size = list(hstack([1, k_size, 1]))
@@ -627,7 +657,6 @@ class NN:
     # *************************** 与 训练有关 的 常用函数 ***************************
 
     ''' 获取 train_op '''
-
     def get_train_op(self, loss, learning_rate, global_step):
         tf.summary.scalar('loss', loss)  # 记录 loss 到 TensorBoard
 
@@ -638,7 +667,6 @@ class NN:
     # ************************* 防止 overfitting 的 trick *************************
 
     ''' 正则化，默认采用 l2_loss 正则化 '''
-
     def regularize(self, loss, beta):
         with tf.name_scope('regularize'):
             regularizer = 0.0
@@ -648,25 +676,24 @@ class NN:
                 regularizer = tf.add(regularizer, tf.nn.l2_loss(W))
             return tf.reduce_mean(loss + beta * regularizer)
 
-    ''' 正则化，默认采用 l2_loss 正则化 '''
 
+    ''' 正则化，默认采用 l2_loss 正则化 '''
     def regularize_trainable(self, loss, beta):
         trainable_var = tf.trainable_variables()
         with tf.name_scope('regularize'):
             regularizer = 0.0
             for var in trainable_var:
-                regularizer = tf.add(regularizer, tf.nn.l2_loss(tf.cast(var, tf.float32)))
+                regularizer = tf.add( regularizer, tf.nn.l2_loss( tf.cast(var, tf.float32) ) )
         return tf.reduce_mean(loss + beta * regularizer)
 
-    ''' dropout '''
 
+    ''' dropout '''
     def dropout(self, a, keep_prob):
         return tf.nn.dropout(a, keep_prob)
 
     # ********************** 其他常用函数 (与 DL 无关) *********************
 
     ''' 输出展示 '''
-
     @staticmethod
     def echo(msg, crlf=True):
         if crlf:
