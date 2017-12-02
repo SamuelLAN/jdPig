7d9f6c364bb2723c92132c89d3f8323dd5ffbd1d
Yusen Lin <412206186@qq.com>
2017-12-02 18:48:48 +0800

    [RC] modify fcn

--- fcn/fcn.py
+++ fcn/fcn.py
@@ -235,11 +235,11 @@ class FCN(base.NN):
     ''' 自定义 初始化变量 过程 '''
     def init(self):
         # 加载数据
-        # self.load()
+        self.load()
 
         # 常量
-        # self.__iter_per_epoch = int(self.__train_size // self.BATCH_SIZE)
-        # self.__steps = self.EPOCH_TIMES * self.__iter_per_epoch
+        self.__iter_per_epoch = int(self.__train_size // self.BATCH_SIZE)
+        self.__steps = self.EPOCH_TIMES * self.__iter_per_epoch
 
         # 输入 与 label
         self.__image = tf.placeholder(tf.float32, [None, None, None, self.NUM_CHANNEL], name='X')
@@ -248,9 +248,9 @@ class FCN(base.NN):
         self.__keep_prob = tf.placeholder(tf.float32, name='keep_prob')
 
         # 随训练次数增多而衰减的学习率
-        # self.__learning_rate = self.get_learning_rate(
-        #     self.BASE_LEARNING_RATE, self.global_step, self.__steps, self.DECAY_RATE, staircase=False
-        # )
+        self.__learning_rate = self.get_learning_rate(
+            self.BASE_LEARNING_RATE, self.global_step, self.__steps, self.DECAY_RATE, staircase=False
+        )
 
         self.__has_rebuild = False
 
@@ -534,6 +534,6 @@ class FCN(base.NN):
         return self.__mask2img(output_mask[0], np_image[0])    # 将 mask 待人 image 并去掉外部的点点
 
 
-# o_fcn = FCN()
-# o_fcn.run()
+o_fcn = FCN()
+o_fcn.run()
 # o_fcn.test_model()
