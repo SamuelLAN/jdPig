a5e3b88fdaf036ec0ccecc5dabd950fb09d77a3e
Yusen Lin <412206186@qq.com>
2017-12-03 10:58:24 +0800

    [RC] modify get_image

--- fcn/base.py
+++ fcn/base.py
@@ -71,10 +71,10 @@ class NN:
 
     ''' 析构函数 '''
     def __del__(self):
-        # pass
-        NN.kill_tensorboard_if_runing()
-        self.tbProcess.join(10)
-        self.tbProcess.terminate()
+        pass
+        # NN.kill_tensorboard_if_runing()
+        # self.tbProcess.join(10)
+        # self.tbProcess.terminate()
 
 
     ''' 初始化 '''
@@ -86,8 +86,8 @@ class NN:
         self.modelPath = ''
         self.get_model_path()                             # 生成存放模型的文件夹 与 路径
 
-        self.__summaryPath = ''
-        self.__get_summary_path()
+        # self.__summaryPath = ''
+        # self.__get_summary_path()
 
         self.global_step = self.get_global_step()            # 记录全局训练状态的 global step
 
