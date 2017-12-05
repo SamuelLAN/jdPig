26b64eed11b354e53409d575eb0830d1189583ba
Yusen Lin <412206186@qq.com>
2017-12-05 18:13:43 +0800

    [RC] modify deep_id

--- deep_id/load.py
+++ deep_id/load.py
@@ -117,7 +117,7 @@ class Download:
 class Data:
     DATA_ROOT = r'data/TrainImgMore'
     # RESIZE = [224, 224]
-    RESIZE = [39, 39]
+    RESIZE = [43, 43]
     RATIO = 1.0
     NUM_CLASSES = 30
 
