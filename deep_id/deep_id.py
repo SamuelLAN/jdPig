f33d4a146ca3007de6501ac0ff2bbce16bf1d446
Yusen Lin <412206186@qq.com>
2017-12-05 20:54:47 +0800

    [RC] modify deep_id

--- deep_id/deep_id.py
+++ deep_id/deep_id.py
@@ -36,10 +36,10 @@ class DeepId(base.NN):
 
     X_LIST_LEN = 6              # 总共有 X_LIST_LEN 个输入，需要训练 X_LIST_LEN 个 CNN
 
-    BASE_LEARNING_RATE = 0.04    # 初始 学习率
-    DECAY_RATE = 0.3            # 学习率 的 下降速率
+    BASE_LEARNING_RATE = 0.03    # 初始 学习率
+    DECAY_RATE = 0.8            # 学习率 的 下降速率
 
-    KEEP_PROB = 0.85            # dropout 的 keep_prob
+    KEEP_PROB = 0.5             # dropout 的 keep_prob
 
     DEEP_ID_LAYER_INDEX = -2    # 倒数第二层为 deep_id 层
 
