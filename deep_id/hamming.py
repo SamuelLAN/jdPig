f918caf59f1980269f928fcd33b8c7c7582dfd88
Yusen Lin <412206186@qq.com>
2017-12-08 02:05:02 +0800

    [RC] modify nn

--- deep_id/hamming.py
+++ deep_id/hamming.py
@@ -11,10 +11,10 @@ from sklearn.neighbors import KNeighborsClassifier
 
 class Hamming:
     DATA_ROOT = r'feature'
-    PCA_DIMENSION = 840
+    PCA_DIMENSION = 420
     # K_NEIGHBORS_LIST = [10, 50, 100, 200, 300, 500, 1000, 2000, 3000, 5000, 10000]
 
-    THRESHOLD = 0.6
+    THRESHOLD = 0.7
 
     def __init__(self):
         self.__deep_id_list = []
