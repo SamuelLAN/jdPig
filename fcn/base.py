b984c858692411fcaf3ad3160c9ddc86de3d9cec
Yusen Lin <412206186@qq.com>
2017-12-09 02:53:13 +0800

    [RC] modify fcn and load

--- fcn/base.py
+++ fcn/base.py
@@ -1,5 +1,6 @@
 #!/usr/bin/Python
 # -*- coding: utf-8 -*-
+from __future__ import print_function
 import tensorflow as tf
 from math import sqrt
 from numpy import hstack
@@ -444,7 +445,7 @@ class NN:
                     NN.cmd('kill -9 %d' % pid)
 
         except Exception, ex:
-            print ex
+            NN.echo(ex)
 
 
     '''
@@ -456,7 +457,7 @@ class NN:
             # NN.cmd('tensorboard --logdir=%s --port=%d' % (path, port))
             NN.cmd('source activate python27;tensorboard --logdir=%s --port=%d' % (path, port))
         except Exception, ex:
-            print ex
+            NN.echo(ex)
 
 
     ''' 异步状态，自动在终端打开 cmd (默认端口为 6006，port 参数可以自己指定端口) '''
@@ -847,7 +848,7 @@ class NN:
     @staticmethod
     def echo(msg, crlf=True):
         if crlf:
-            print msg
+            print (msg)
         else:
             sys.stdout.write(msg)
             sys.stdout.flush()
