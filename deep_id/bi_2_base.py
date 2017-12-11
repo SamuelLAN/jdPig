87dfcb68764d28a777c7b594511129186fe9f6e2
Yusen Lin <412206186@qq.com>
2017-12-11 19:16:03 +0800

    [RC] modify bi_vgg

--- deep_id/bi_2_base.py
+++ deep_id/bi_2_base.py
@@ -162,9 +162,7 @@ class NN:
 
         var_list = tf.trainable_variables()
         g_list = tf.global_variables()
-        bn_moving_vars = [g for g in g_list if 'moving_mean' in g.name]
-        bn_moving_vars += [g for g in g_list if 'moving_variance' in g.name]
-        var_list += bn_moving_vars
+        var_list += g_list
         self.saver = tf.train.Saver(var_list=var_list)
 
 
